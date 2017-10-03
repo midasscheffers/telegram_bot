dc23193334b788609ab26035e7d856abd0e57e51
Midas Scheffers <midasscheffers@gmail.com>
2017-10-03 09:44:03 +0200

    Try to make it work

--- bot.js
+++ bot.js
@@ -1,12 +1,3 @@
-/*const express = require('express');
-const app = express()
-
-var server = app.listen(3000);
-
-app.use(express.static('public'));
-
-console.log("de server is running")
-*/
 const TeleBot = require('telebot');
 
 const bot = new TeleBot({
